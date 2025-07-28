fe630c44cca9c0576c222f051e76baaeb144316d
-g <imran.geekologix@gmail.com>
2025-07-28 15:44:51 +0530

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
@@ -7,26 +8,24 @@ const Header = () => {
                 <div className='text-[32px] text-[#FF4F00] font-semibold'>Gausetu</div>
                 <div>By Shreenath Gaushala</div>
             </div>
-            <div className=''>
-                <ul className='flex gap-2 items-center justify-between'>
-                    <li>
-                        Home
-                    </li>
-                    <li>
-                        About Us
-                    </li>
-                    <li>
-                        Events
-                    </li>
-                    <li>
-                        Support the Cows
-                    </li>
-                    <li>
-                        Contact with us
-                    </li>
+            <ul className='flex gap-6 font-[500] items-center justify-between'>
+                <li>
+                  <Link href={'/'}>Home</Link>  
+                </li>
+                <li>
+                  <Link href={'/about'}> About Us</Link> 
+                </li>
+                <li>
+                 <Link href={'/events'}>Events</Link>   
+                </li>
+                <li>
+                  <Link href={'/support'}>Support the Cows</Link>  
+                </li>
+                <li>
+                 <Link href={'/contact'}>Contact with us</Link>   
+                </li>
 
-                </ul>
-            </div>
+            </ul>
             <div className=''>
                 <button className='text-white bg-[#FF4F00] p-2 rounded-full'>Download</button>
             </div>
